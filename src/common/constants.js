c20b067b27ffea6f4c7b46452ed34c3211363e81
cecsun <ceciliesunde94@gmail.com>
2025-04-08 09:09:41 +0200

    Added a folder with API

---
+++ src/common/constants.js
@@ -0,0 +1,4 @@
+const API_BASE_URL = 'https://v2.api.noroff.dev';
+
+export const API_VENUES_URL = `${API_BASE_URL}/holidaze/venues`;
+export const API_SINGLE_VENUE_URL = `${API_BASE_URL}/holidaze/venues/<id>`;
