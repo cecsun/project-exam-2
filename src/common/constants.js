39018c7a117493a9d324e9f04b6129edbecc3a77
cecsun <ceciliesunde94@gmail.com>
2025-04-08 12:42:54 +0200

    Changed the names of the constants

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,4 @@
 const API_BASE_URL = 'https://v2.api.noroff.dev';
 
-export const API_VENUES_URL = `${API_BASE_URL}/holidaze/venues`;
-export const API_SINGLE_VENUE_URL = `${API_BASE_URL}/holidaze/venues/<id>`;
+export const API_PRODUCTS_URL = `${API_BASE_URL}/holidaze/venues`;
+export const API_SINGLE_PRODUCT_URL = `${API_BASE_URL}/holidaze/venues/<id>`;
