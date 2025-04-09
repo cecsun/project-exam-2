04aeaae3d70d07d80c472c1fe4e1b2a0c8b5d13f
cecsun <ceciliesunde94@gmail.com>
2025-04-09 08:21:13 +0200

    Updated API url

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,4 @@
 const API_BASE_URL = 'https://v2.api.noroff.dev';
 
 export const API_PRODUCTS_URL = `${API_BASE_URL}/holidaze/venues`;
-export const API_SINGLE_PRODUCT_URL = `${API_BASE_URL}/holidaze/venues/<id>`;
+export const API_SINGLE_PRODUCT_URL = `${API_BASE_URL}/holidaze/venues`;
