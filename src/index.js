7b54308f42cf2bd86e60af8598fb72f99de739f4
cecsun <ceciliesunde94@gmail.com>
2025-04-22 09:32:24 +0200

    Added BrowserRouter and AuthProvider around App

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './context/CartContext';
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <CartProvider>
+    <AuthProvider>
       <App />
-    </CartProvider>
+    </AuthProvider>
   </BrowserRouter>
 );
 
