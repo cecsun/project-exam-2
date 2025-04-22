ed7bfe6f378b72f16904f52aadf1d69d059704f9
cecsun <ceciliesunde94@gmail.com>
2025-04-22 09:31:10 +0200

    Created routes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from "react-router-dom"
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
-import RegisterLogin from './pages/RegisterLoginPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import SingleProductPage from './pages/SingleProductPage';
+import ProfilePage from './pages/ProfilePage';
 import RouteNotFound from './pages/RouteNotFound';
+// import ProtectedRoute from './components/ProtectedRoute/index.js';
 
 function App() {
   return (
@@ -14,9 +18,20 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="register" element={<RegisterLogin />} />
-          <Route path="login" element={<RegisterLogin />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="/product/:id" element={<SingleProductPage />} />
+          <Route path="profile" element={<ProfilePage />} />
           <Route path="*" element={<RouteNotFound />} />
+          
+          {/* <Route
+            path="/HomePage"
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          /> */}
         </Route>
       </Routes>
     </div>
