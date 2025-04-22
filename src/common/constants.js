59716c929a5d9978aa510cef9e437e8e75bb4c61
cecsun <ceciliesunde94@gmail.com>
2025-04-22 08:59:57 +0200

    Added more constants

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,9 @@
 const API_BASE_URL = 'https://v2.api.noroff.dev';
 
+export const API_LOGIN_URL = `${API_BASE_URL}/auth/login`;
+export const API_REGISTER_URL = `${API_BASE_URL}/auth/register`;
 export const API_PRODUCTS_URL = `${API_BASE_URL}/holidaze/venues`;
 export const API_SINGLE_PRODUCT_URL = `${API_BASE_URL}/holidaze/venues`;
+export const API_PROFILE_URL = `${API_BASE_URL}/holidaze/profiles`;
+export const API_BOOKINGS_URL = `${API_BASE_URL}/holidaze/bookings`;
+export const API_KEY = "857d70c0-ecb1-48e9-82d3-7609d2c35475";
