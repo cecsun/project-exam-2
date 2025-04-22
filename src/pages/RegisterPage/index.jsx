cabbfd04db3f20bdb904857abd6467f165b9f0da
cecsun <ceciliesunde94@gmail.com>
2025-04-22 09:45:03 +0200

    Pages ready with imported register and login form

---
+++ src/pages/RegisterPage/index.jsx
@@ -0,0 +1,16 @@
+// import React, { useState } from 'react';
+// import { Container, Form, Button, Alert } from 'react-bootstrap';
+// import { useNavigate } from 'react-router-dom';
+
+import React from 'react';
+import RegisterForm from '../../components/Register/RegisterForm';
+
+function RegisterPage() {
+  return (
+    <div>
+        <RegisterForm />
+    </div>
+  );
+}
+
+export default RegisterPage;
